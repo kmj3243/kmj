25d864adc6c6498ead25ee4c903d621306229669
k3 <k3@k3-PC>
2017-11-09 15:21:08 +0900

    create println('H')

--- git_test/src/git_test/Main.java
+++ git_test/src/git_test/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
 	public static void main(String[] args){
 		System.out.println('A');
-		
+		System.out.println('B');
+		System.out.println('C');
+		System.out.println('D');
+		System.out.println('E');
+		System.out.println('G');
+		System.out.println('H');
 	}
 }
